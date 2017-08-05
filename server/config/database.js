74fae29c26d7b5d2778695c2dd5b9fe4c8793d15
Khoa Nguyen <knb.nguyen8@gmail.com>
2017-08-05 12:38:42 -0700

    [prog] routes for business

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,95 @@
 const elasticsearch = require('elasticsearch');
 
+const Promise = require('bluebird')
 const client = new elasticsearch.Client({
   host: 'http://ec2-54-162-18-40.compute-1.amazonaws.com:9200'
 });
 
+const myClient = Promise.promisifyAll(client);
+
 client.ping({
     requestTimeout: 1000
   }, error => {
-    error ? console.trace('elasticsearch cluster is down!', error) : console.log('Elastic search is connected'); 
+    error ? console.trace('elasticsearch cluster is down!', error) : init(); 
 });
 
+const init = () => {
+  console.log("Elastic Search is connected!");
+  console.log("Creating index...");
+  const store = [];
+  myClient.indices.exists({
+    index: 'checkins'
+  })
+  .then(data => {
+    if(data && !data.error_message){
+      console.log("Mapping index...", data);
+      return myClient.indices.putMapping({
+          index: 'checkins',
+          type: 'businesses',
+          body: {
+            "properties" : {
+                "name" : { "type" : "string"},
+                "full_address" : { "type" : "string"},
+                "total_checkins" : { "type" : "integer"}  
+            }
+          }
+      })
+    }else if(data.error_message){
+      return createIndex()
+    }
+  })
+  .then(data => {
+    console.log(data);
+    if(data && !data.error_message){
+       return myClient.search({
+          index: 'khoa_2017_08_05_index',
+          type: 'business',
+          q: "*"
+        })
+    }
+  })
+  .then(data => {
+   // console.log(data);
+    if(data && !data.error_message){
+      console.log('test');
+       let hits = data.hits.hits;
+        hits.forEach(business => {
+          let tmp = {};
+          tmp.id = business._id;
+          tmp.name = business._source.name;
+          tmp.full_address = business._source.full_address;
+          tmp.total_checkins = business._source.checkin_info ? countCheckin(business._source.checkin_info) : 0
+          let def = { index:  { _index: 'checkins', _type: 'businesses', _id: business._id } }
+          store.push(def)
+          store.push(tmp);
+        });
+        //console.log(store)
+        return myClient.bulk({
+          body: store
+        })
+    }
+  })
+  .then(data => {
+    if(data){
+        console.log("inserted documents", data.took);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
-module.exports = client;
+  
+  console.log("Getting existing data from business");
+  //calculateCheckin();
+}
+
+
+const countCheckin = (checkInObj) => {
+  let total = 0;
+  for(let key in checkInObj){
+    total += checkInObj[key]
+  }
+  return total;
+}
+
+module.exports = myClient;
--- server/controllers/Elastic.js
+++ server/controllers/Elastic.js
@@ -4,8 +4,8 @@ const client = require('../config/database.js');
 module.exports = {
   query: function (req, res, next) {
     client.search({
-      index: 'khoa_2017_08_05_index',
-      type: 'business',
+      index: 'checkins',
+      type: 'businesses',
       q: "*"
     }).then(function (resp) {
         var hits = resp.hits.hits;
@@ -17,6 +17,39 @@ module.exports = {
 
   getBusinesses: function (req, res, next) {
     console.log(req.query);
-    res.json(req.query);
+    let query = req.query.q ? req.query.q : '*';
+    let size = req.query.size ? req.query.size : 10;
+    let page = req.query.page ? req.query.page : 0;
+    if(size < 0 || page < 0){
+      res.status(400).json({"error": "Invalid request"})
+    }
+    client.search({
+      index: 'checkins',
+      type: 'businesses',
+      q: `${query}&page=${page}&size=${size}`
+    })
+    .then(data => {
+      res.json(data)
+    })
+    //res.json(req.query);
+  },
+
+  getBusinessesWithCheckins: function (req, res, next) {
+    client.search({
+      index: 'khoa_2017_08_05_index',
+      type: 'business',
+      query: {
+        body: {
+          match: {
+            
+          }
+        }
+      }
+    }).then(function (resp) {
+        var hits = resp.hits.hits;
+        res.json(hits)
+    }, function (err) {
+        console.trace(err.message);
+    });
   }
 }
